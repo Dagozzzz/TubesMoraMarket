ccc820f67bb8d45db514d26839d4ea78a333cc3d
Florssalt <bungaramadhanisudana@example.com>
2026-05-12 15:57:01 +0700

    pull

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -6,13 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-<<<<<<< HEAD
     
-=======
-    /**
-     * Run the migrations.
-     */
->>>>>>> 26e97f2025c755b96589f8a20891e02a0db0ccd4
     public function up(): void
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
