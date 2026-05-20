8a59bd70cad8eef2ca3eb75ffc4070e52bb1bb2e
chaerunnisyadara-maker <you@example.com>
2026-05-20 15:32:48 +0700

    status

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -22,14 +22,21 @@ class Barang extends Model
         parent::boot();
 
         static::creating(function ($barang) {
-            $lastBarang = self::orderBy('id', 'desc')->first();
-
-            if ($lastBarang) {
-                $lastNumber = (int) substr($lastBarang->kode_barang, 3);
-                $newNumber = $lastNumber + 1;
-            } else {
-                $newNumber = 1;
-            }
+            $barang->kode_barang = self::generateKodeBarang();
         });
+    }
+
+    public static function generateKodeBarang()
+    {
+        $lastBarang = self::orderBy('id', 'desc')->first();
+
+        if ($lastBarang && $lastBarang->kode_barang) {
+            $lastNumber = (int) substr($lastBarang->kode_barang, 3);
+            $newNumber = $lastNumber + 1;
+        } else {
+            $newNumber = 1;
+        }
+
+        return 'BRG' . str_pad($newNumber, 3, '0', STR_PAD_LEFT);
     }
 }
